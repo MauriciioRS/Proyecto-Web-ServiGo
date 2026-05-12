5b7826d4569595d151e5e86068043a072e4999a9
Leonardo Ramos <davidleo.dlrc@gmail.com>
2026-05-12 09:57:51 -0500

    Agrego detalles de servicio html y el controller

--- src/main/java/com/ServiGo/servigo/controller/ServicioController.java
+++ src/main/java/com/ServiGo/servigo/controller/ServicioController.java
@@ -1,13 +1,13 @@
 package com.ServiGo.servigo.controller;
 
-import com.ServiGo.servigo.model.Servicio;
-import com.ServiGo.servigo.repository.ServicioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ServiGo.servigo.repository.ServicioRepository;
 
 @Controller
 @RequestMapping("/servicios")
@@ -32,3 +32,4 @@ public class ServicioController {
         return "redirect:/servicios";
     }
 }
+ 
